a2601dd0b806f58b21a9a247cde738b68a1e25b0
saulack <saulack@users.noreply.github.com>
2026-06-03 21:18:39 -0400

    Fix app settings jsonb migration

--- supabase/migrations/0006_app_settings_jsonb.sql
+++ supabase/migrations/0006_app_settings_jsonb.sql
@@ -1,6 +1,12 @@
 alter table app_settings
+  alter column value drop default;
+
+alter table app_settings
   alter column value type jsonb
   using to_jsonb(value);
+
+alter table app_settings
+  alter column value set default to_jsonb(false);
 
 insert into app_settings (key, value)
 values ('homepage_reservations_count', to_jsonb(5))
